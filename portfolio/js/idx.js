e418dc7cb09ddeb52044196ffbfe5470cb7edb46
XI ZHANG <x7zhang@gmail.com>
2016-11-06 22:13:32 -0500

    Add files via upload

---
+++ portfolio/js/idx.js
@@ -0,0 +1,21 @@
+$("document").ready(function() {
+  handleTyping();
+});
+
+
+
+
+
+
+function handleTyping () {
+  $(".element").typed({
+    strings: ["I'm a new grad Web Developer, love cook and code."],
+    typeSpeed: 50,
+    starDelay: 200,
+    backDelay: 600,
+    loop: false,
+    showCursor: false,
+    cursorChar: "|"
+  });
+}
+
